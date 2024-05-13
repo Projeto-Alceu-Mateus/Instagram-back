e647c1b1d40324142b9d9c3cc00eeed45963cdef
Mateus C. <mateus.c000@outlook.com>
2024-05-13 19:36:16 -0300

    Criação da classe updateDTO

---
+++ src/main/java/com/instagram/clone/dto/UserUpdateRequestDTO.java
@@ -0,0 +1,35 @@
+package com.instagram.clone.dto;
+
+public class UserUpdateRequestDTO {
+
+    private String email;
+    private String fullName;
+    private String bio;
+    private String profilePicture;
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getBio() {
+        return bio;
+    }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+    
+}
