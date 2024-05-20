a846d1dd929e416572042380bc77a7eeab2f74ca
alceu1 <alceu.junior@alunos.sc.senac.br>
2024-05-19 21:18:01 -0300

    remove Bugs

--- src/main/java/com/instagram/clone/controller/CommentController.java
+++ src/main/java/com/instagram/clone/controller/CommentController.java
@@ -1,5 +1,7 @@
 package com.instagram.clone.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,5 +20,11 @@ public class CommentController {
     public ResponseEntity<CommentDTO> createComment(@RequestBody CommentDTO commentDTO) {
         CommentDTO createdComment = commentService.createComment(commentDTO);
         return ResponseEntity.ok(createdComment);
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<CommentDTO>> getCommentsByPostId(@PathVariable Long postId) {
+        List<CommentDTO> comments = commentService.getCommentsByPostId(postId);
+        return ResponseEntity.ok(comments);
     }
 }
